be26ee09ff5c69229fd0afd10338d21594e5bbea
sasisingavarapu <sasisingavarapu@gmail.com>
2025-04-19 17:19:05 +0530

    update

--- ui/asset_manager/load_asset_manager.py
+++ ui/asset_manager/load_asset_manager.py
@@ -31,9 +31,11 @@ from functools import partial
 import glob
 
 try:
-   import importlib;from importlib import reload
+    import importlib
+    from importlib import reload
 except:
-    import imp;from imp import reload
+    import imp
+    from imp import reload
 
 import SakitechX_Asset_Manager
 from SakitechX_Asset_Manager.utils.asset_manager import files_manager
@@ -43,7 +45,7 @@ reload(files_manager)
 TITLE = 'SakitechX Asset Manager'
 UI_FILE = 'asset_manager.ui'
 FULL_PATH = os.path.dirname(__file__)
-QT_PATH =os.path.join(FULL_PATH, UI_FILE)
+QT_PATH = os.path.join(FULL_PATH, UI_FILE)
 
 
 def maya_main_window():
@@ -58,7 +60,6 @@ class SakitechXAssetManager(QtWidgets.QMainWindow):
         self.setWindowTitle(TITLE)
 
         self.cFileManager = files_manager.FileManager()
-
         self.stage_tab = None
 
         self.init_ui()
@@ -66,7 +67,6 @@ class SakitechXAssetManager(QtWidgets.QMainWindow):
         self.create_connections()
 
     def init_ui(self):
-        # Load UI file
         loader = QUiLoader()
         file = QtCore.QFile(QT_PATH)
         file.open(QtCore.QFile.ReadOnly)
@@ -74,37 +74,33 @@ class SakitechXAssetManager(QtWidgets.QMainWindow):
         file.close()
 
     def create_layout(self):
-        self.resize(800,400)
-        self.check_path_exits()
+        self.resize(800, 400)
+        self.check_path_exists()
         self.put_path_in_edit_line()
         self.populate_asset_layout()
 
     def create_connections(self):
         self.ui.select_path_btn.clicked.connect(self.edit_path_line)
 
-    def check_path_exits(self):
-
+    def check_path_exists(self):
         if not os.path.exists(self.cFileManager.path):
             self.ask_window_path()
-
 
     def ask_window_path(self):
         dialog = cmds.fileDialog2(dialogStyle=1, fileMode=3, caption="Open")
         if not dialog:
             return False
-        path_folder = dialog[0]
 
+        path_folder = dialog[0]
         write_json = os.path.join(self.cFileManager.config_path)
 
         data = {
             "scenes_path": path_folder
-
         }
         with open(write_json, 'w') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
         self.cFileManager.path = path_folder
-
         return path_folder
 
     def put_path_in_edit_line(self):
@@ -119,22 +115,17 @@ class SakitechXAssetManager(QtWidgets.QMainWindow):
 
     def populate_asset_layout(self):
         main_layout = self.ui.asset_vlayout
-
         assets = self.cFileManager.asset
         print('Assets:', assets)
 
         for asset_path in assets:
-            bassename = self.get_basename(asset_path)
-            folder_button = QtWidgets.QPushButton(bassename)
+            basename = self.get_basename(asset_path)
+            folder_button = QtWidgets.QPushButton(basename)
+            folder_button.setFixedSize(150, 40)
 
-            folder_button.setFixedSize(150,40)
-
+            stage = self.get_stages(asset_path)
+            folder_button.clicked.connect(partial(self.create_tab_stage, stage))
             main_layout.addWidget(folder_button)
-
-        # Connet button
-        stage = self.get_stages(asset_path)
-        folder_button.clicked.connect(partial(self.create_tab_stage, stage))
-
 
     def get_stages(self, path):
         self.cFileManager.current_asset = path
@@ -142,13 +133,12 @@ class SakitechXAssetManager(QtWidgets.QMainWindow):
         return asset_stages
 
     def clean_assets_layout(self):
-         ''
+        pass
 
-    def create_tab_stage(self, stages_dic ):
-
+    def create_tab_stage(self, stages_dic):
         if self.stage_tab:
             self.stage_tab.setParent(None)
-            self.stage_tab=None
+            self.stage_tab = None
 
         self.stage_tab = QtWidgets.QTabWidget()
         self.ui.asset_details.addWidget(self.stage_tab)
@@ -157,11 +147,30 @@ class SakitechXAssetManager(QtWidgets.QMainWindow):
             tab = QtWidgets.QWidget()
             layout = QtWidgets.QVBoxLayout(tab)
             for data in stages_dic[stage]:
-                lable = QtWidgets.QLabel(data)
-                layout.addWidget(lable)
+                if not self.filter_by_ext(path=data):
+                    continue
+                label = QtWidgets.QLabel(data)
+                layout.addWidget(label)
             self.stage_tab.addTab(tab, stage)
 
+    def filter_by_ext(self, path):
+        current_ext = self.ui.ext_combo.currentText()
+        if current_ext:
+            current_ext = current_ext.replace('.', '')
+        path_extension = path.split('.')[-1]
 
+        print(current_ext)
+        print(path_extension)
+
+        if path_extension == 'ma':
+            path_extension = 'mb'
+
+        if current_ext == 'Show_All':
+            return True
+        elif current_ext.lower() == path_extension.lower():
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
@@ -173,6 +182,3 @@ if __name__ == "__main__":
 
     cSakitechXAssetManager = SakitechXAssetManager()
     cSakitechXAssetManager.show()
-
-
-
